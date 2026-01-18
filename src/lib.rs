56a66bf899fb1c73754ed4593a5f2ea7f5096262
Swarnit <ghuleswarnit@gmail.com>
2026-01-18 13:00:54 +0000

    added greedy beam search

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
+use ordered_float::OrderedFloat;
 #[warn(unused)]
 // #[warn(dead_code)]
 use rayon::prelude::*;
-
 use std::cmp::Ordering;
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashMap, HashSet};
 
 // const N
 
@@ -43,24 +45,121 @@ impl VectorStore {
     }
 }
 
-// pub struct HNSW {
-//     layers: Vec<Vec<Vec<usize>>>,
-//     // Use diffrent approach this is not okay , its fast very complex thing ! !! !
-//     // Just forst implement the HNSW or implement with care so you can  understand properly
-//     // That not a clean and neat apraoch there exists better appraoches !
-// }
+pub struct GraphLayers {
+    base_layer: Vec<Vec<usize>>,
+    upper_layers: Vec<HashMap<usize, Vec<usize>>>,
+}
 
-// impl HNSW {
-//     pub fn insert(&mut self, q: &[f32], m: usize, m_max: usize, efConstruction: usize, m_l: f32) {
-//         // update hnsw inserting element q
-//         todo!()
-//     }
+struct Distibution {}
 
-//     pub fn search_layer(q: &[f32], ep: Vec<usize>, ef: usize, lc: usize) -> Vec<usize> {
-//         //  ef: Vec<usize> = [node_ids] closest neighbors to q
-//         todo!()
-//     }
-// }
+impl GraphLayers {
+    fn add_neighbors(&mut self) {
+        // TO-DO
+    }
+
+    fn add_edge(&mut self, node_id_1: usize, node_id_2: usize, layer: usize, d: bool) {
+        // Check if thye both exsist in the same layer ? ! :(
+        if layer > 0 {
+            // Handle the upper layer addedge logic ! i GUESS WE DONT NEED THIS
+            // self.upper_layers[layer-1].get(&node_id_1);
+        } else {
+            self.base_layer[node_id_1].push(node_id_2);
+            if !d {
+                self.base_layer[node_id_2].push(node_id_1);
+            }
+        }
+    }
+
+    fn insert_node(&mut self) -> usize {
+        let node_id = self.base_layer.len(); // index + 1 
+        self.base_layer.push(Vec::new());
+        node_id
+    }
+
+    fn insert_node_upper_layers(&mut self, layer: usize, node_id: usize) {
+        self.upper_layers[layer - 1].insert(node_id, Vec::new());
+    }
+
+    fn get_neighbors(&self, layer: usize, node_id: usize) -> &[usize] {
+        if layer == 0 {
+            return &self.base_layer[node_id];
+        } else {
+            if layer <= self.upper_layers.len() {
+                match self.upper_layers[layer - 1].get(&node_id) {
+                    Some(slice) => slice,
+                    None => &[],
+                }
+            } else {
+                return &[];
+            }
+        }
+    }
+
+    fn new() -> Self {
+        // Distrubution of nodes in the upper layer by exponetioal decay probalistic function
+        // Choose random nodes and promote them to upper layers
+        todo!()
+    }
+}
+
+pub struct HNSW {
+    pub layers: GraphLayers,
+    pub vectors: VectorStore,
+    pub entry_point: Option<usize>,
+    pub max_level: usize,
+    pub ef_construction: usize,
+    pub m: usize,
+}
+
+impl HNSW {
+    pub fn insert(&mut self, q: &[f32], m: usize, m_max: usize, efConstruction: usize, m_l: f32) {
+        todo!()
+    }
+
+    // greedy beam search
+    pub fn search_layer(&self, q: &[f32], lc: usize) -> Vec<usize> {
+        let ep = self.entry_point.expect("ENTRY POINT ERROR");
+        let sq_dist = VectorStore::squared_distance_to_query(&self.vectors, ep, q);
+        // Candidates is Min Que
+        let mut candidates: BinaryHeap<Reverse<(OrderedFloat<f32>, usize)>> = BinaryHeap::new(); // (Dist , node_id)
+        // Found Neighbors is Max Que
+        let mut found_neighbours: BinaryHeap<(OrderedFloat<f32>, usize)> = BinaryHeap::new();
+        let mut visited: HashSet<usize> = HashSet::new();
+
+        visited.insert(ep);
+        candidates.push(Reverse((OrderedFloat(sq_dist), ep)));
+        found_neighbours.push((OrderedFloat(sq_dist), ep));
+
+        while !candidates.is_empty() {
+            let Reverse((OrderedFloat(dist_c), closest_candidate)) = candidates.pop().unwrap();
+
+            let (OrderedFloat(dist_worst), furthest_element) = *found_neighbours.peek().unwrap();
+
+            if dist_c > dist_worst {
+                break;
+            }
+
+            for e in GraphLayers::get_neighbors(&self.layers, lc, closest_candidate) {
+                if !visited.contains(e) {
+                    let dist_e = VectorStore::squared_distance_to_query(&self.vectors, *e, q);
+                    let dist_e_wrapped = OrderedFloat(dist_e);
+
+                    let (OrderedFloat(current_worst_dist), _) = *found_neighbours.peek().unwrap();
+                    visited.insert(*e);
+                    if dist_e < current_worst_dist || found_neighbours.len() < self.ef_construction
+                    {
+                        candidates.push(Reverse((OrderedFloat(dist_e), *e)));
+                        found_neighbours.push((OrderedFloat(dist_e), *e));
+                        if found_neighbours.len() > self.ef_construction {
+                            found_neighbours.pop();
+                        }
+                    }
+                }
+            }
+        }
+        found_neighbours.into_iter().map(|(_, idx)| idx).collect()
+    }
+}
 
 #[derive(Debug)]
 // pub struct Graph {
